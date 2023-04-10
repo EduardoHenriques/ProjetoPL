5fad98481c0d6b7c2faa023296af5dd48e39caba
EduardoHenriques <fernando28henriques@gmail.com>
2023-04-10 16:52:02 +0100

    Work on lexer

--- src/lexer.py
+++ src/lexer.py
@@ -1,4 +1,13 @@
 import ply.lex as lex
+import re
+
+level = 0
+comm_level = 0
+indi_level = 0
+birth_level = 0
+death_level = 0
+burial_level = 0
+marriage_level = 0
 
 
 def t_error(t):
@@ -6,17 +15,26 @@ def t_error(t):
 
 
 def lexer_gen():
+	states = [
+		("comm", "inclusive"),
+		("birth", "inclusive"),
+		("death", "inclusive"),
+		("burial", "inclusive"),
+		("marriage", "inclusive"),
+		("individual", "inclusive"),
+	]
+
 	tokens = [
 		"BARRA",
 
 		"LEVEL",
 
 		"HEADER",
-		"SOUR",	 # approved system ID
-		"DEST",	 #
+		"SOUR",  # approved system ID
+		"DEST",  #
 		"FILE",
 		"CHAR",
-		
+
 		"COMM",
 		"CONT",
 
@@ -31,7 +49,7 @@ def lexer_gen():
 		"DATE",
 		"BIRTH",
 		"DEATH",
-		"MAR",	 # casamento
+		"MAR",  # casamento
 		"PLACE",
 		"BURIAL",
 
@@ -43,19 +61,17 @@ def lexer_gen():
 		"CHILD",
 		"HUSBAND",
 		"WIFE",
-		"DIV",	 # divorcio(opcional, T/F)
-
-
+		"DIV",  # divorcio(opcional, T/F)
 
 	]
+
+	# expressoes tokens
 
 	t_ignore = r"\t\s\n"
 
 	t_POINTER = r"\@\w+[^@]?\@"
 
-	t_LEVEL = r"^\d+"
-
-	t_ID = t_LEVEL + r"\s*" + t_POINTER + r"\s*(?=INDI)"
+	t_ID = r"^\d+" + r"\s*" + t_POINTER + r"\s*(?=INDI)"
 
 	t_NAME = r"(?<=NAME).+"
 
@@ -66,10 +82,105 @@ def lexer_gen():
 
 	t_SEX = r"(?<=SEX)[MF]"
 
-	t_BURIAL = r"(?<=BURI).+"
+	t_BIRTH = "BIRT"
+	t_DEATH = "DEAT"
+	t_BURIAL = r"BURI"
+	t_MAR = r"MARR"
 
 	t_PLACE = r"(?<=PLAC).+"
 
+	# FUNCOES PARA ESTADOS
+
+	def t_LEVEL(t):
+		r"""^(\d+)"""
+		global level
+		level = int(t.value)
+		return t
+
+	# individuo
+	def t_OpenIndividual(t):
+		r"""\s*\@\w+[^@]?\@\s*(?=INDI)"""
+		global indi_level, level
+		indi_level = level
+		t.lexer.push_state("individual")
+
+	def t_individual_CloseIndividual(t):
+		r"""^\d+"""
+		global level, indi_level
+		level = int(t.value)
+		if indi_level >= level:
+			t.lexer.pop_state()
+			print("---")
+
+	# comentario
+	def t_OpenComm(t):
+		r"""COMM"""
+		global comm_level, level
+		comm_level = level
+		t.lexer.push_state("comm")
+
+	def t_comm_CloseComm(t):
+		r"""^\d+"""
+		global level, comm_level
+		level = int(t.value)
+		if comm_level >= level:
+			t.lexer.pop_state()
+
+	# birth
+	def t_OpenBirth(t):
+		r"""BIRT"""
+		global birth_level
+		birth_level = level
+		t.lexer.push_state("birth")
+
+	def t_birth_CloseBirth(t):
+		r"""^\d+"""
+		global level, birth_level
+		level = int(t.value)
+		if birth_level >= level:
+			t.lexer.pop_state()
+
+	# death
+	def t_OpenDeath(t):
+		r"""DEAT"""
+		global death_level
+		death_level = level
+		t.lexer.push_state("death")
+
+	def t_death_CloseDeath(t):
+		r"""^\d+"""
+		global level, death_level
+		level = int(t.value)
+		if death_level >= level:
+			t.lexer.pop_state()
+
+	# burial
+	def t_OpenBurial(t):
+		r"""BURI"""
+		global burial_level
+		burial_level = level
+		t.lexer.push_state("burial")
+
+	def t_burial_CloseBurial(t):
+		r"""^\d+"""
+		global level, burial_level
+		level = int(t.value)
+		if burial_level >= level:
+			t.lexer.pop_state()
+
+	# marriage
+	def t_OpenMarriage(t):
+		r"""MARR"""
+		global marriage_level
+		marriage_level = level
+		t.lexer.push_state("marriage")
+
+	def t_marriage_CloseMarriage(t):
+		r"""^\d+"""
+		global level, marriage_level
+		level = int(t.value)
+		if marriage_level >= level:
+			t.lexer.pop_state()
 
 	return lex.lex()
 
@@ -77,8 +188,8 @@ def lexer_gen():
 if __name__ == "__main__":
 	lexer = lexer_gen()
 	with open("test/teste.txt", "r") as file:
-		linhas = file.readlines(30000)
+		linhas = file.readlines(6000)
 		for line in linhas:
 			lexer.input(line)
 			for token in lexer:
-				print(f"TIPO: {token.type:<10} | VALOR: {token.value:<10}")
+				print(f"TIPO: {token.type:<10} | VALOR: {token.value:<50} | ESTADO: {lexer.current_state():<10} (NIVEL {level})")
